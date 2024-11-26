2fb9412b9e112ec4cbb618f89ee6de9878988278
Abdel <abdulrashidissah232@gmail.com>
2024-11-26 11:36:29 +0000

    added more translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,16 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
   title = 'translateApp';
-  languages = ['tr', 'en','de','fr'];
+  languages = ['tr', 'en','de','fr','ru'];
   
   private translate = inject(TranslateService);
+  blogPosts = [
+    { title: 'post_1_title', content: 'post_1_content' },
+    { title: 'post_2_title', content: 'post_2_content' },
+    { title: 'post_3_title', content: 'post_3_content' },
+    { title: 'post_4_title', content: 'post_4_content' },
+    // { title: 'post_5_title', content: 'post_5_content' },
+  ];
   
   ngOnInit(): void {
     const defaultLang = localStorage.getItem('language') || 'de';
